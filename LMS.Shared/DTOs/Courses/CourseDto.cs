4524838c467efcaee7c01bfe0d7a3f5e25b1cf61
zhelyazkovse <zhelyazkovse@gmail.com>
2025-09-04 11:09:20 +0200

    added course/activity/module/overbview dto, client imports update

---
+++ LMS.Shared/DTOs/Courses/CourseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs.Courses
+{
+    public record CourseDto
+    {
+        public int Id { get; init; }
+        public string Title { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public DateTime StartDate { get; init; }
+        public DateTime EndDate { get; init; }
+    }
+
+}
