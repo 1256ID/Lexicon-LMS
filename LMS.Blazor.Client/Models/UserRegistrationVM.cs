40b55f0b8e32b753d599a9cadf15073df9597add
Sarbast Hesso <sarbast.hesso@gmail.com>
2025-09-10 14:18:34 +0200

    UserService in frontend

---
+++ LMS.Blazor.Client/Models/UserRegistrationVM.cs
@@ -0,0 +1,15 @@
+﻿namespace LMS.Blazor.Client.Models
+{
+    public class UserRegistrationVM
+    {
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
+
